5c18bf837818d4baf234a3b8f754a173441e5c1f
John Zhao <techzjc@vt.edu>
2025-11-13 02:27:59 -0500

    feat: 🎸 ASCII-art & site info
    Add ASCII art and predefined the info to make it easier to show within
    the console

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { execSync } from 'child_process'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __GIT_COMMIT__: JSON.stringify(
+      execSync('git rev-parse --short HEAD').toString().trim()
+    ),
+    __COMPILE_TIME__: JSON.stringify(
+      new Date().toISOString()
+    ),
+  },
 })
