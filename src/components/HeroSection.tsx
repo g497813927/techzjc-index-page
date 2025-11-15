a88be87868b4f7127507752ff7a9344139abfa2f
John Zhao <techzjc@vt.edu>
2025-11-15 00:18:01 -0500

    style: 💄 Hero background change display
    Move hero background to img to make it lazy load

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,8 +3,11 @@ import './HeroSection.css';
 export function HeroSection() {
     return (
         <section className="hero-section">
+            <div className="background-image">
+                <img src="/assets/image/hero-image.webp" alt="Hero Background" loading="lazy" />
+            </div>
             <div className="background-overlay"></div>
-            <h1>TECHZJC</h1>
+            <h1 id="hero-text">TECHZJC</h1>
             <button type="button" className="move-down-indicator" onClick={() => {
                 window.scrollBy({
                     top: window.innerHeight,
