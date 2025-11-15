46d467f4c3d48ad4995f9f17516133641822e957
John Zhao <techzjc@vt.edu>
2025-11-15 17:54:48 -0500

    fix: 🐛 Next JS stylesheet issue

--- apps/web-next/src/app/page.tsx
+++ apps/web-next/src/app/page.tsx
@@ -2,7 +2,6 @@ import { HeroSection } from '@/components/HeroSection'
 import { NavBar } from '@/components/NavBar'
 import { Publications } from '@/components/Publications';
 import { Footer } from '@/components/Footer'
-import './App.css'
 import { MoveToTop } from '@/components/MoveToTop'
 import { About } from '@/components/About';
 import { PhotoWall } from '@/components/PhotoWall';
