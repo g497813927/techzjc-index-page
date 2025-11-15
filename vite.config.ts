f82b237364dd6574988383c29c715a5f59091311
John Zhao <techzjc@vt.edu>
2025-11-15 16:34:10 -0500

    ci: 🎡 Remove rerouting to static site as it is not necessary
    Remove rerouting resources to static site as it can be delivered through
    FC/Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,5 @@ export default defineConfig({
       new Date().toISOString()
     ),
     __VERCEL_ENV__: JSON.stringify(process.env.VERCEL_ENV || ''),
-  },
-  base: process.env.NODE_ENV === 'production' && process.env.SERVER_FULL === 'true'
-    ? 'https://static.techzjc.com/index/'
-    : '/',
+  }
 })
