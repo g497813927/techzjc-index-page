ab450a6ac8af2acffa5dfdec427674a71122ff22
John Zhao <techzjc@vt.edu>
2025-11-17 04:57:54 -0500

    style: 💄 Change to section to make higher accessibility

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFoundError({ error, _ }: { error: Error; _: () => voi
   return (
     <>
     <NavBar hasHero={false} />
-    <div className="page-body center-content column-content not-found-page">
+    <section className="page-body center-content column-content not-found-page">
       <FontAwesomeIcon icon={faTriangleExclamation} className="warning-icon" size="6x" width="6em" height="6em" />
       <h1 className="error-404-page-title">Page Not Found!</h1>
       <h2>The page you are looking for does not exist.</h2>
@@ -19,7 +19,7 @@ export default function NotFoundError({ error, _ }: { error: Error; _: () => voi
       <Link href="/" className="back-home-link">
       <FontAwesomeIcon icon={faChevronLeft} className="back-home-icon" size="1x" width="1em" height="1em" />
       Go back to Home Page</Link>
-    </div>
+    </section>
     <Footer />
     </>
   );
