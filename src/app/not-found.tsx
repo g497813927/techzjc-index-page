26480225cfdd73f4b78b2325f6f0554fca69a32a
John Zhao <techzjc@vt.edu>
2025-11-17 04:54:39 -0500

    fix: 🐛 layout shift for warning icon

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -12,12 +12,12 @@ export default function NotFoundError({ error, _ }: { error: Error; _: () => voi
     <>
     <NavBar hasHero={false} />
     <div className="page-body center-content column-content not-found-page">
-      <FontAwesomeIcon icon={faTriangleExclamation} className="warning-icon" width="6em" height="6em" />
+      <FontAwesomeIcon icon={faTriangleExclamation} className="warning-icon" size="6x" width="6em" height="6em" />
       <h1 className="error-404-page-title">Page Not Found!</h1>
       <h2>The page you are looking for does not exist.</h2>
       <br />
       <Link href="/" className="back-home-link">
-      <FontAwesomeIcon icon={faChevronLeft} className="back-home-icon" width="1em" height="1em" />
+      <FontAwesomeIcon icon={faChevronLeft} className="back-home-icon" size="1x" width="1em" height="1em" />
       Go back to Home Page</Link>
     </div>
     <Footer />
