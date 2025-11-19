99de87bde141fa0606ccd2134a9ac928a90322fa
John Zhao <techzjc@vt.edu>
2025-11-18 21:18:34 -0500

    fix: 🐛 Comment rendering issue for bots
    Now comments section hides to reduce soft 404 issue for bots

--- src/components/blog/CommentSection.tsx
+++ src/components/blog/CommentSection.tsx
@@ -1,14 +1,16 @@
 "use client";
-import dynamic from 'next/dynamic';
+import Giscus from '@giscus/react';
 import './CommentSection.css';
 import { useEffect, useState } from 'react';
 
-export default function CommentSection() {
+export default function CommentSection() {    
     const [theme, setTheme] = useState<"light" | "dark">(
         typeof window !== "undefined" && document.documentElement.getAttribute("data-theme") === "dark"
             ? "dark"
             : "light"
     );
+    const [ giscusStatus, setGiscusStatus ] = useState<"loading" | "loaded" | "error">("loading");
+
     useEffect(() => {
         const observer = new MutationObserver(() => {
             const currentTheme = document.documentElement.getAttribute("data-theme");
@@ -17,6 +19,37 @@ export default function CommentSection() {
         observer.observe(document.documentElement, { attributes: true, attributeFilter: ['data-theme'] });
         return () => observer.disconnect();
     }, []);
+
+    useEffect(() => {
+        // Try to fetch Giscus API first to check if there are any restrictions (e.g., Googlebot being blocked from accessing it)
+        if (typeof window === "undefined") return;
+        if (process.env.NEXT_PUBLIC_VERCEL_ENV === "true" && giscusStatus === "loading") {
+        try {
+            // Get the current page title for Giscus term
+            const term = document.title;
+            fetch(`https://giscus.app/api/discussions?repo=techzjc%2Fblog-discussions&term=${encodeURIComponent(term)}&category=Announcements`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                },
+            }).then((response) => {
+                if (response.ok) {
+                    setGiscusStatus("loaded");
+                } else {
+                    setGiscusStatus("error");
+                }
+            }).catch(() => {
+                setGiscusStatus("error");
+            });
+        // eslint-disable-next-line
+        } catch (error) {
+            // eslint-disable-next-line
+            setGiscusStatus("error");
+        }
+      } else {
+        setGiscusStatus("error");
+      }
+    }, [giscusStatus]);
     if (process.env.NODE_ENV !== "production") {
         return (
             <em className='comment-section'>
@@ -30,7 +63,7 @@ export default function CommentSection() {
             </em>
         )
     } else {
-        const Giscus = dynamic(() => import('@giscus/react').then((mod) => mod.default), { ssr: false });
+        
         return (
             <Giscus
                 id="comments"
