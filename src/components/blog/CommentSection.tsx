449a6072a6a334f93fa7a2d0dc3759936669d39a
John Zhao <techzjc@vt.edu>
2025-11-18 21:45:27 -0500

    fix: 🐛 Comment section bot loading
    Forces not to load any giscus based on bot UAs

--- src/components/blog/CommentSection.tsx
+++ src/components/blog/CommentSection.tsx
@@ -1,15 +1,20 @@
 "use client";
+import { headers } from 'next/headers';
 import Giscus from '@giscus/react';
 import './CommentSection.css';
 import { useEffect, useState } from 'react';
 
-export default function CommentSection() {    
+function isBot(ua: string) {
+    return /(bot|yandex|spider|slurp)/i.test(ua);
+}
+
+export default function CommentSection() {
     const [theme, setTheme] = useState<"light" | "dark">(
         typeof window !== "undefined" && document.documentElement.getAttribute("data-theme") === "dark"
             ? "dark"
             : "light"
     );
-    const [ giscusStatus, setGiscusStatus ] = useState<"loading" | "loaded" | "error">("loading");
+
 
     useEffect(() => {
         const observer = new MutationObserver(() => {
@@ -20,36 +25,6 @@ export default function CommentSection() {
         return () => observer.disconnect();
     }, []);
 
-    useEffect(() => {
-        // Try to fetch Giscus API first to check if there are any restrictions (e.g., Googlebot being blocked from accessing it)
-        if (typeof window === "undefined") return;
-        if (process.env.NEXT_PUBLIC_VERCEL_ENV === "true" && giscusStatus === "loading") {
-        try {
-            // Get the current page title for Giscus term
-            const term = document.title;
-            fetch(`https://giscus.app/api/discussions?repo=techzjc%2Fblog-discussions&term=${encodeURIComponent(term)}&category=Announcements&strict=false&last=1`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                },
-            }).then((response) => {
-                if (response.ok || response.status === 404) {
-                    setGiscusStatus("loaded");
-                } else {
-                    setGiscusStatus("error");
-                }
-            }).catch(() => {
-                setGiscusStatus("error");
-            });
-        // eslint-disable-next-line
-        } catch (error) {
-            // eslint-disable-next-line
-            setGiscusStatus("error");
-        }
-      } else {
-        setGiscusStatus("error");
-      }
-    }, [giscusStatus]);
     if (process.env.NODE_ENV !== "production") {
         return (
             <em className='comment-section'>
@@ -63,29 +38,33 @@ export default function CommentSection() {
             </em>
         )
     } else {
-        if (giscusStatus === "loading") {
-            return null;
-        } else if (giscusStatus === "error") {
-            return (
-                <em className='comment-section'>
-                    Comment section is currently unavailable.
-                </em>
-            )
-        }
-        return (
-            <Giscus
-                id="comments"
-                repo="techzjc/blog-discussions"
-                repoId="R_kgDOQYYKNw"
-                category="Announcements"
-                categoryId="DIC_kwDOQYYKN84Cx8L7"
-                mapping="title"
-                reactionsEnabled="1"
-                emitMetadata="1"
-                inputPosition="top"
-                theme={theme === "dark" ? "transparent_dark" : "light"}
-                loading="lazy"
-            />
-        );
+        return headers().then((headers) => {
+            const ua = headers.get('user-agent') || '';
+            return isBot(ua);
+        }).then((is_bot_ua) => {
+            if (is_bot_ua) {
+                return (
+                    <em className='comment-section'>
+                        Comment section is unavailable
+                    </em>
+                )
+            } else {
+                return (
+                    <Giscus
+                        id="comments"
+                        repo="techzjc/blog-discussions"
+                        repoId="R_kgDOQYYKNw"
+                        category="Announcements"
+                        categoryId="DIC_kwDOQYYKN84Cx8L7"
+                        mapping="title"
+                        reactionsEnabled="1"
+                        emitMetadata="1"
+                        inputPosition="top"
+                        theme={theme === "dark" ? "transparent_dark" : "light"}
+                        loading="lazy"
+                    />
+                );
+            }
+        });
     }
 }
