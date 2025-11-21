27341e3bdc936268fba47f6999818f9b5e8bbb28
John Zhao <techzjc@vt.edu>
2025-11-21 11:39:55 -0500

    fix: 🐛 remove Metadata from not_found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,28 +5,7 @@ import { NavBar } from "@/components/NavBar";
 import { faChevronLeft, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import { Metadata } from 'next';
 import "./not-found.css";
-
-export const metadata: Metadata = {
-  title: "Page Not Found - Techzjc",
-  description: "The page you are looking for does not exist.",
-  keywords: ["techzjc", "科技ZJC网", "ZJC科技网", "Techzjc", "ZJC", "赵佳成", "g497813927", "Jiacheng Zhao", "John Zhao", "404", "page not found"],
-  icons: {
-    icon: "https://static.techzjc.com/icon/favicon_index_page.ico",
-    apple: [
-      {
-        url: "https://static.techzjc.com/icon/bookmark_icon_index_page.png",
-        sizes: "180x180",
-        type: "image/png"
-      }
-    ],
-    shortcut: "https://static.techzjc.com/icon/favicon_index_page.ico"
-  },
-  alternates: {
-    canonical: `https://techzjc.com/404`,
-  },
-}
 
 export default function NotFoundError({  }: { error: Error; _: () => void }) {
   return (
