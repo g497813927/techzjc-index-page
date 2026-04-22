e1759be35cfff892fb61972eb827ba6bfa4be03e
John Zhao <techzjc@vt.edu>
2026-04-21 23:24:11 -0400

    Restore root 404 global styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import { getNotFoundPageData } from "./not-found-locale";
 import { NotFoundContent } from "./not-found-shared";
+import "./globals.css";
 import "./[lang]/not-found.css";
 
 export default async function RootNotFound() {
